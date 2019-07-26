59bc7c5057711362e41cbcaa6ce40aa7b5316946
Kelsi H <kelsimhoyle@gmail.com>
2019-07-26 08:39:54 -0600

    redoing to match class activities

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,5 @@
+### Schema
+
 CREATE DATABASE burger_db;
 USE burger_db;
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,6 @@
-INSERT INTO burgers (name)
-VALUES ("Bacon"), ("Cheeseburger"), ("Original Hambuger");
-
-INSERT INTO burgers (name, devoured) 
-VALUES ("Animal Style", true);
+INSERT INTO burgers (name) VALUES ('Meeses');
+INSERT INTO burgers (name) VALUES ('Bobbi');
+INSERT INTO burgers (name, sleepy) VALUES ('Sylvester', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Marilyn Manson', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Joe Biden', true);
+INSERT INTO burgers (name) VALUES ('Owens');
