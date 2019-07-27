a044f6f7e19b50add0e41038fec50582683cb09d
Kelsi H <kelsimhoyle@gmail.com>
2019-07-27 16:52:44 -0600

    Past burger list fixed with handlebars

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burgers (name) VALUES ('She’s A Super Leek Burger'), ("Hit Me With Your Best Shallot Burger"), ("Papaya Was a Rolling Stone Burger");
+INSERT INTO burgers (name) VALUES ('She’s A Super Leek Burger'), ("Hit Me With Your Best Shallot Burger"), ("Papaya Was a Rolling Stone Burger"), ('Foot Feta-ish Burger');
 
-INSERT INTO burgers (name, devoured) VALUES ('Foot Feta-ish Burger', true), ("Chile Relleno-You-Didn’t Burger", true), ("Open Sesame Burger ", true);
+INSERT INTO burgers (name, devoured) VALUES ("Chile Relleno-You-Didn’t Burger", true), ("Open Sesame Burger ", true);
 
